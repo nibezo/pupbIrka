87a040e21465b86176edf99847a9a5ae22ace88e
nibezo <nibezo.cs@gmail.com>
2023-04-29 01:46:32 +0400

    fix: add CORSMiddleware

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Set up CORS
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
